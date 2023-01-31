7bee235b57ea19e814af25b528febf86eb597279
tomori <tomori.study@gmail.com>
2023-01-31 00:40:10 +0000

    navbar color modify

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::check())
     {{-- ユーザ一覧ページへのリンク --}}
-    <li><a class="link link-hover" href="#">Users</a></li>
+    <li><a class="link link-hover text-black" href="#">Users</a></li>
     {{-- ユーザ詳細ページへのリンク --}}
-    <li><a class="link link-hover" href="#">{{ Auth::user()->name }}&#39;s profile</a></li>
+    <li><a class="link link-hover text-black" href="#">{{ Auth::user()->name }}&#39;s profile</a></li>
     <li class="divider lg:hidden"></li>
     {{-- ログアウトへのリンク --}}
-    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+    <li><a class="link link-hover text-black" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
 @else
     {{-- ユーザ登録ページへのリンク --}}
     <li><a class="link link-hover text-black" href="{{ route('register') }}">会員登録</a></li>
